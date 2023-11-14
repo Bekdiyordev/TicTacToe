ccb012ccf18e5ec42830ed3f057a21cda3dd5b48
Beko404 <abekdiyor@gmail.com>
2023-11-14 18:06:10 +0500

    system files

--- app/src/main/java/uz/beko404/tictactoe/ui/Game.kt
+++ app/src/main/java/uz/beko404/tictactoe/ui/Game.kt
@@ -1,21 +1,266 @@
 package uz.beko404.tictactoe.ui
 
+import android.app.AlertDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.size
+import androidx.core.widget.doAfterTextChanged
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.imageview.ShapeableImageView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import uz.beko404.tictactoe.BaseFragment
 import uz.beko404.tictactoe.R
 import uz.beko404.tictactoe.databinding.FragmentGameBinding
+import uz.beko404.tictactoe.utils.SharedPref
 import uz.beko404.tictactoe.utils.viewBinding
 
 class Game : BaseFragment(R.layout.fragment_game) {
+    private lateinit var sharedPref: SharedPref
     private val binding by viewBinding { FragmentGameBinding.bind(it) }
+    private val board = Array(3) { IntArray(3) { 0 } }
+    private var isPlayerTurn = true
+    private var playerScoreCount = 0
+    private var opponentScoreCount = 0
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        sharedPref = SharedPref(requireContext())
         setupUI()
     }
 
-    private fun setupUI() = with(binding){
+    private fun setupUI() = with(binding) {
+        playerName.text = sharedPref.username
+        opponentName.text = sharedPref.opponent
+
+        p11.setOnClickListener {
+            itemClick(it)
+        }
+        p12.setOnClickListener {
+            itemClick(it)
+        }
+        p13.setOnClickListener {
+            itemClick(it)
+        }
+        p21.setOnClickListener {
+            itemClick(it)
+        }
+        p22.setOnClickListener {
+            itemClick(it)
+        }
+        p23.setOnClickListener {
+            itemClick(it)
+        }
+        p31.setOnClickListener {
+            itemClick(it)
+        }
+        p32.setOnClickListener {
+            itemClick(it)
+        }
+        p33.setOnClickListener {
+            itemClick(it)
+        }
+    }
+
+    private fun itemClick(view: View) {
+        val tag = view.tag.toString()
+        if (checkPlace(tag = tag)) {
+            if (isPlayerTurn) {
+                (view as ShapeableImageView).setImageResource(R.drawable.ic_x)
+                board[tag.substring(0, 1).toInt()][tag.substring(1).toInt()] = 1
+                binding.playerTurn.visibility = View.INVISIBLE
+                binding.opponetTurn.visibility = View.VISIBLE
+            } else {
+                (view as ShapeableImageView).setImageResource(R.drawable.ic_o)
+                board[tag.substring(0, 1).toInt()][tag.substring(1).toInt()] = 2
+                binding.playerTurn.visibility = View.VISIBLE
+                binding.opponetTurn.visibility = View.INVISIBLE
+            }
+
+            isPlayerTurn = !isPlayerTurn
+            checkGameFinished()
+        } else
+            Toast.makeText(requireContext(), "This place is busy", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun checkGameFinished() {
+        if (isDraw())
+            binding.result.visibility = View.VISIBLE
+        else {
+            if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] != 0) findWinner(
+                1
+            )
+            if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] != 0) findWinner(
+                2
+            )
+            if (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] != 0) findWinner(
+                3
+            )
+            if (board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] != 0) findWinner(
+                4
+            )
+            if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] != 0) findWinner(
+                5
+            )
+            if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] != 0) findWinner(
+                6
+            )
+            if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != 0) findWinner(
+                7
+            )
+            if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != 0) findWinner(
+                8
+            )
+        }
+
+    }
+
+    private fun isDraw(): Boolean {
+        for (i in 0..2) {
+            for (j in 0..2)
+                if (board[i][j] == 0)
+                    return false
+        }
+        return true
+    }
+
+    private fun findWinner(i: Int) {
+        when (i) {
+            1 -> {
+                showResult(board[0][0])
+                binding.winLine.visibility = View.VISIBLE
+                binding.line1.visibility = View.VISIBLE
+            }
+
+            2 -> {
+                showResult(board[1][0])
+                binding.winLine.visibility = View.VISIBLE
+                binding.line2.visibility = View.VISIBLE
+            }
+
+            3 -> {
+                showResult(board[2][0])
+                binding.winLine.visibility = View.VISIBLE
+                binding.line3.visibility = View.VISIBLE
+            }
+
+            4 -> {
+                showResult(board[0][0])
+                binding.winLine.visibility = View.VISIBLE
+                binding.winLine.rotation = -90f
+                binding.line2.visibility = View.VISIBLE
+            }
+
+            5 -> {
+                showResult(board[0][1])
+                binding.winLine.visibility = View.VISIBLE
+                binding.winLine.rotation = -90f
+                binding.line2.visibility = View.VISIBLE
+            }
+
+            6 -> {
+                showResult(board[0][2])
+                binding.winLine.visibility = View.VISIBLE
+                binding.winLine.rotation = -90f
+                binding.line3.visibility = View.VISIBLE
+            }
+
+            7 -> {
+                showResult(board[1][1])
+                binding.winLine.visibility = View.VISIBLE
+                binding.winLine.rotation = 45f
+                binding.line2.visibility = View.VISIBLE
+
+            }
+
+            8 -> {
+                showResult(board[1][1])
+                binding.winLine.visibility = View.VISIBLE
+                binding.winLine.rotation = -45f
+                binding.line2.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun showDialog(winnerName: String) {
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+        val inflater = this.layoutInflater
+        val dialogView = inflater.inflate(R.layout.dialog_result, null)
+        dialogBuilder.setView(dialogView)
+        val b = dialogBuilder.create()
+        b.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        dialogView.findViewById<TextView>(R.id.winner_name).text = winnerName
+
+        val usernameView = dialogView.findViewById<TextView>(R.id.username)
+        usernameView.text = username
+        val button = dialogView.findViewById<MaterialButton>(R.id.save)
+        button.isEnabled = !usernameView.text.isNullOrEmpty()
+
+        usernameView.doAfterTextChanged {
+            button.isEnabled = !it.isNullOrEmpty()
+        }
+
+        button.setOnClickListener {
+            if (state) {
+                sharedPref.username = usernameView.text.toString()
+                binding.playerName.text = sharedPref.username
+            } else {
+                sharedPref.opponent = usernameView.text.toString()
+                findNavController().navigate(R.id.action_home_to_game)
+            }
+            b.dismiss()
+        }
+        b.show()
+    }
+
+    private fun showResult(type: Int) = with(binding) {
+        if (type == 1) {
+            playerScoreCount++
+            playerScore.text = playerScoreCount.toString()
+        } else {
+            opponentScoreCount++
+            opponentScore.text = opponentScoreCount.toString()
+        }
+
+        lifecycleScope.launch {
+            delay(1000)
+            withContext(Dispatchers.Main) {
+                if (type == 1)
+                    showDialog(sharedPref.username)
+                else
+                    showDialog(sharedPref.opponent)
+            }
+        }
+
+    }
+
+    private fun checkPlace(tag: String): Boolean {
+        return board[tag.substring(0, 1).toInt()][tag.substring(1).toInt()] == 0
+    }
+
+    private fun resetGame() = with(binding) {
+        p11.setImageResource(R.drawable.placeholder)
+        p12.setImageResource(R.drawable.placeholder)
+        p13.setImageResource(R.drawable.placeholder)
+        p21.setImageResource(R.drawable.placeholder)
+        p22.setImageResource(R.drawable.placeholder)
+        p23.setImageResource(R.drawable.placeholder)
+        p31.setImageResource(R.drawable.placeholder)
+        p32.setImageResource(R.drawable.placeholder)
+        p33.setImageResource(R.drawable.placeholder)
+        line1.visibility = View.INVISIBLE
+        line2.visibility = View.INVISIBLE
+        line3.visibility = View.INVISIBLE
+        winLine.visibility = View.GONE
 
     }
 }
